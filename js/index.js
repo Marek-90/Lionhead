8b1af9a5c1a4bb1eb6bbf65a075ecb6c0ee94d20
Marek Sieńkowski <mareksienkowski.90@gmail.com>
2020-09-18 19:54:56 +0200

    form

---
+++ js/index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    const goForm = document.querySelector(".main__popup");
+    const mainForm = document.querySelector(".popup-container")
+
+    goForm.addEventListener("click", function (e) {
+        e.preventDefault();
+        mainForm.style.display = "flex";
+    })
+
+
+});
