db139219ab98d9e82936c0595513fba581771087
Marek Sieńkowski <mareksienkowski.90@gmail.com>
2020-09-19 14:44:16 +0200

    complete

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
 
+
     const goForm = document.querySelector(".main__popup");
     const mainForm = document.querySelector(".popup-container")
+
 
     goForm.addEventListener("click", function (e) {
         e.preventDefault();
